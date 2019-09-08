bd4734ecda1698a9979852dfa5fa27708ee948e6
Ender <xenderpugx@gmail.com>
2019-09-08 16:16:50 +0100

    updates to code

--- src/devtools.js
+++ src/devtools.js
@@ -1,9 +1,32 @@
-fetch("devtools.css")
+fetch("https://devtools-dot.enderdev.now.sh/serve_file/devtools.css")
   .then(res => res.text())
   .then(data => {
     chrome.devtools.panels.applyStyleSheet(data);
-    console.log(chrome.devtools);
-    chrome.devtools.panels.setOpenResourceHandler(resource, () => {
-      console.log(resource);
-    });
+    console.clear();
+    console.log(
+      "%c Developer Tools %c Main %c Injected stylesheet",
+      "background: #1d1d1d; border-radius: 3px 0px 0px 3px; color: white; display: block;",
+      "background: black; border-radius: 0px; color: white; display: block;",
+      "background: #dbdbdb; border-radius: 0px 3px 3px 0px; color: black; display: block;"
+    );
+    console.log(
+      "%c Developer Tools %c Socket %c Preparing websocket",
+      "background: #1d1d1d; border-radius: 3px 0px 0px 3px; color: white; display: block;",
+      "background: black; border-radius: 0px; color: white; display: block;",
+      "background: #dbdbdb; border-radius: 0px 3px 3px 0px; color: black; display: block;"
+    );
+    console.log(
+      `%c Developer Tools %c Main %c Websocket connected to tab ${chrome.devtools.inspectedWindow.tabId}`,
+      "background: #1d1d1d; border-radius: 3px 0px 0px 3px; color: white; display: block;",
+      "background: black; border-radius: 0px; color: white; display: block;",
+      "background: #dbdbdb; border-radius: 0px 3px 3px 0px; color: black; display: block;"
+    );
+    console.log(
+      `%c Developer Tools %c Info %c Using ${
+        chrome.devtools.panels.themeName == "default" ? "light" : "dark"
+      } theme`,
+      "background: #1d1d1d; border-radius: 3px 0px 0px 3px; color: white; display: block;",
+      "background: black; border-radius: 0px; color: white; display: block;",
+      "background: #dbdbdb; border-radius: 0px 3px 3px 0px; color: black; display: block;"
+    );
   });
