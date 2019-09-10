a669032083ed1266ed70d906a2005009a23c6bd1
EnderDev <endercraftergaming@gmail.com>
2019-09-10 21:17:53 +0100

    Improvements to Settings page

--- src/devtools.js
+++ src/devtools.js
@@ -1,4 +1,4 @@
-fetch("https://devtools-dot.enderdev.now.sh/serve_file/devtools.css")
+fetch("devtools.css")
   .then(res => res.text())
   .then(data => {
     chrome.devtools.panels.applyStyleSheet(data);
